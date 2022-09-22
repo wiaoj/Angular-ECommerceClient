078f1b7da51b2c9f6783b86d4fdecaa11a2f2de9
wiaoj <bertandeniz7@gmail.com>
2022-09-23 02:39:35 +0300

    Added reactive form user register component

--- src/app/components/ui/components/register/register.component.ts
+++ src/app/components/ui/components/register/register.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { User } from 'src/app/entities/user';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +19,52 @@ export class RegisterComponent implements OnInit {
   form: FormGroup;
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      nameSurname: [''],
-      userName: [''],
-      email: [''],
-      password: [''],
-      password2: [''],
-    });
+    this.form = this.formBuilder.group(
+      {
+        //2. dizi içinde validasyonlar tanımlanıyor
+        nameSurname: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(50),
+            Validators.minLength(3),
+          ],
+        ],
+        userName: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(50),
+            Validators.minLength(3),
+          ],
+        ],
+        email: [
+          '',
+          [Validators.required, Validators.maxLength(250), Validators.email],
+        ],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: (group: AbstractControl): ValidationErrors | null => {
+          let password = group.get('password').value;
+          let confirmPassword = group.get('confirmPassword').value;
+
+          return password === confirmPassword ? null : { notSame: true };
+        },
+      }
+    );
   }
 
-  onSubmit(data: any) {}
+  //property olduğu için component şeklinde kullanılabilir hale geliyo
+  get component() {
+    return this.form.controls;
+  }
+
+  submitted: boolean = false;
+  onSubmit(data: User) {
+    this.submitted = true;
+    
+    if (this.form.invalid) return;
+  }
 }
