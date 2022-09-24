2cb9d57b82772a18ddbe9cf9e60c3920999305db
wiaoj <bertandeniz7@gmail.com>
2022-09-24 03:59:31 +0300

    added token model

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/TokenResponse';
 import { Create_User } from 'src/app/contracts/user/Create_User';
 import { User } from 'src/app/entities/user';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../ui/customToastr/custom-toastr.service';
 import { HttpClientService } from '../httpClient/http-client.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClientService: HttpClientService) {}
+  constructor(
+    private httpClientService: HttpClientService,
+    private toastrService: CustomToastrService
+  ) {}
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> =
@@ -25,8 +35,10 @@ export class UserService {
     usernameOrEmail: string,
     password: string,
     callBackFunction?: () => void
-  ): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post(
+  ): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<
+      any | TokenResponse
+    >(
       {
         controller: 'users',
         action: 'login',
@@ -37,7 +49,17 @@ export class UserService {
       }
     );
 
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+      localStorage.setItem('accessToken', tokenResponse.token.accessToken);
+      console.log(tokenResponse);
+      
+
+      this.toastrService.message('Success', 'Success', {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopFullWidth,
+      });
+    }
     callBackFunction();
   }
 }
